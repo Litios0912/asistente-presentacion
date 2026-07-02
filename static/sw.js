8a4cbf08a252c2e94c996f8dde844c642f1ca4cb
Litios0912 <litios0912@users.noreply.github.com>
2026-07-02 10:43:54 -0600

    Fix: force SW cache refresh with new version

--- static/sw.js
+++ static/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "asistente-v1";
+const CACHE = "asistente-v2";
 const FILES = ["/", "/index.html", "/css/style.css", "/js/app.js", "/js/speech.js", "/js/upload.js", "/js/presentation.js", "/manifest.json"];
 
 self.addEventListener("install", (e) => {
@@ -7,6 +7,11 @@ self.addEventListener("install", (e) => {
 });
 
 self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))
+    )
+  );
   e.waitUntil(clients.claim());
 });
 
